3df2633e3c3cc4fd5c7f497a7c9ea970a2f860b2
yuen-kai <cykai168@gmail.com>
2025-03-02 16:17:13 -0500

    something

--- store.ts
+++ store.ts
@@ -9,11 +9,15 @@ interface SettingsStore {
 
 const useSettingsStore = create<SettingsStore>((set) => ({
     places: [
-        { id: 1, location: "Central Park", coords: { latitude: 40.785091, longitude: -73.968285 }, start: new Date(), end: new Date(), duration: 120 },
-        { id: 2, location: "Eiffel Tower", coords: { latitude: 48.858844, longitude: 2.294351 }, start: new Date(), end: new Date(), duration: 90 },
-        { id: 3, location: "Great Wall of China", coords: { latitude: 40.431908, longitude: 116.570374 }, start: new Date(), end: new Date(), duration: 180 },
-      ],
-    setPlaces: (newPlaces) => set((state) => ({ places: newPlaces })),
+        { id: 1, location: "Cadillac Mountain Summit", coords: { latitude: 44.3385, longitude: -68.2733 }, start: new Date(), end: new Date(), duration: 180 },
+        { id: 2, location: "Jordan Pond", coords: { latitude: 44.3636, longitude: -68.2387 }, start: new Date(), end: new Date(), duration: 120 },
+        { id: 3, location: "Thunder Hole", coords: { latitude: 44.3012, longitude: -68.2108 }, start: new Date(), end: new Date(), duration: 90 },
+        { id: 4, location: "Sand Beach", coords: { latitude: 44.2300, longitude: -68.3000 }, start: new Date(), end: new Date(), duration: 60 },
+        { id: 5, location: "Otter Cliff", coords: { latitude: 44.2500, longitude: -68.2600 }, start: new Date(), end: new Date(), duration: 75 },
+        { id: 6, location: "Bass Harbor Head Light", coords: { latitude: 44.2850, longitude: -68.3800 }, start: new Date(), end: new Date(), duration: 50 },
+        { id: 7, location: "Jordan Bay", coords: { latitude: 44.3700, longitude: -68.2200 }, start: new Date(), end: new Date(), duration: 110 },
+    ],
+    setPlaces: (newPlaces) => set(() => ({ places: newPlaces })),
 }));
 
 export default useSettingsStore;
