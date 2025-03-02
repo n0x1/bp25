9a4bf559be3915e9276a5d92bde8ecb7876841d8
cam m <71352417+n0x1@users.noreply.github.com>
2025-03-02 09:44:30 -0500

    mmmm

--- components/map.tsx
+++ components/map.tsx
@@ -34,11 +34,7 @@ export default function Map() {
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flex: 1,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
