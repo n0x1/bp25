09f1bd7435ccd66b64bb31e5a96e96c455043ead
yuen-kai <cykai168@gmail.com>
2025-03-02 13:51:49 -0500

    stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Map from "@/components/map";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { Place } from '@/types';
+import { Card } from 'react-native-paper';
 
 export default function Index() {
   const [origin, setOrigin] = useState<{ latitude: number; longitude: number } | null>(null);
   const [destination, setDestination] = useState<{ latitude: number; longitude: number } | null>(null);
   const vehicle = 'car'; // Change this to 'car', 'bike', or 'foot' as needed
+  const [places, setPlaces] = useState<Place[]>([
+      { id: 1, location: "Central Park", coords: { latitude: 40.785091, longitude: -73.968285 }, start: new Date(), end: new Date(), duration: 120 },
+      { id: 2, location: "Eiffel Tower", coords: { latitude: 48.858844, longitude: 2.294351 }, start: new Date(), end: new Date(), duration: 90 },
+      { id: 3, location: "Great Wall of China", coords: { latitude: 40.431908, longitude: 116.570374 }, start: new Date(), end: new Date(), duration: 180 },
+    ]);
 
   const geoapifyApiKey = 'b75c0428362b4b0f973d03d2638cb08c';
 
@@ -42,6 +49,19 @@ export default function Index() {
           <Map origin={origin} destination={destination} vehicle={vehicle} />
         )}
       </View>
+      <FlatList
+        data={places}
+        style={{ flex: 1 }}
+        renderItem={({ item }) => 
+        <Card style={{ margin: 16 }} key={item.id} onPress={()=>{setEditPlace(item); setModalVisible(true)}}>
+          <Card.Title title={item.location} />
+          <Card.Content>
+            <Text>Avalible: {item.start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} to {item.end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Text>
+            <Text>Duration: {item.duration} minutes</Text>
+          </Card.Content>
+        </Card>}
+        // contentContainerStyle={{backgroundColor: "red" }}
+      />
     </View>
   );
 }
